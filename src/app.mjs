2887bfdd93779146f70552e6681be0a4ca77facc
ishanpranav <ishan.pranav@outlook.com>
2024-07-03 12:43:39 -0700

    Template

--- src/app.mjs
+++ src/app.mjs
@@ -3,10 +3,17 @@
 // Licensed under the MIT license.
 
 import express from 'express';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const port = process.env.PORT || 3001;
+const rootDirectory = dirname(fileURLToPath(import.meta.url));
+const bootstrapDirectory = join(rootDirectory, '../node_modules/bootstrap/dist/');
+const publicDirectory = join(rootDirectory, 'public');
 
 express()
+    .use(express.static(publicDirectory))
+    .use(express.static(bootstrapDirectory))
     .get('/', (request, response) => {
         response.json('{}');
     })
